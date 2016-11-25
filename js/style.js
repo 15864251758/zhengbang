c231009853932aab092e0ead1a7ddc9ae044d1c2
❤ Zhl <1229184708@qq.com>
2016-11-25 09:26:49 +0800

    调整了导航条下滑问题和jQuery脚本问题

--- js/style.js
+++ js/style.js
@@ -46,8 +46,6 @@ $(function(){
                 var stayPos = document.createElement("div");
      		    stayPos.id= "stayPosDiv";
 				stayPos.style.height = "38px";//导航条的高度
-     		    var temp = document.getElementById("nav");
-        		temp.appendChild(stayPos);
         		test=true;
     		}
         }else{
@@ -122,162 +120,50 @@ $(function(){
     })
 
     //about
-        jQuery('.tp-banner1').revolution({
-            delay: 9000,
-            startwidth: 1170,
-            startheight: 800,
-            hideThumbs: 30,
-            fullWidth: "on",
-            forceFullWidth: "on",
-            navigationType:"none",
+    jQuery('.tp-banner1').revolution({
+        delay: 9000,
+        startwidth: 1170,
+        startheight: 800,
+        hideThumbs: 30,
+        fullWidth: "on",
+        forceFullWidth: "on",
+        navigationType:"none",
+    });
+
+        $(".header_default .dropdown-menu").hover(function(event){
+         event.stopPropagation();
+          event.preventDefault();
         });
+
 
 })
 
-	$(window).scroll(function(){
-		var numt=$('.fight').offset().top;
-		var numh=$(window).scrollTop();
-		if (numh>=1200 & numh<=1300) {
-			//数字翻滚
-				$.fn.countTo = function (options) {
-					options = options || {};
-					
-					return $(this).each(function () {
-						// set options for current element
-						var settings = $.extend({}, $.fn.countTo.defaults, {
-							from:            $(this).data('from'),
-							to:              $(this).data('to'),
-							speed:           $(this).data('speed'),
-							refreshInterval: $(this).data('refresh-interval'),
-							decimals:        $(this).data('decimals')
-						}, options);
-						
-						// how many times to update the value, and how much to increment the value on each update
-						var loops = Math.ceil(settings.speed / settings.refreshInterval),
-							increment = (settings.to - settings.from) / loops;
-						
-						// references & variables that will change with each update
-						var self = this,
-							$self = $(this),
-							loopCount = 0,
-							value = settings.from,
-							data = $self.data('countTo') || {};
-						
-						$self.data('countTo', data);
-						
-						// if an existing interval can be found, clear it first
-						if (data.interval) {
-							clearInterval(data.interval);
-						}
-						data.interval = setInterval(updateTimer, settings.refreshInterval);
-						
-						// initialize the element with the starting value
-						render(value);
-						
-						function updateTimer() {
-							value += increment;
-							loopCount++;
-							
-							render(value);
-							
-							if (typeof(settings.onUpdate) == 'function') {
-								settings.onUpdate.call(self, value);
-							}
-							
-							if (loopCount >= loops) {
-								// remove the interval
-								$self.removeData('countTo');
-								clearInterval(data.interval);
-								value = settings.to;
-								
-								if (typeof(settings.onComplete) == 'function') {
-									settings.onComplete.call(self, value);
-								}
-							}
-						}
-						
-						function render(value) {
-							var formattedValue = settings.formatter.call(self, value, settings);
-							$self.html(formattedValue);
-						}
-					});
-				};
-				
-				$.fn.countTo.defaults = {
-					// from: 899,               // the number the element should start at
-					// to: 0,                 // the number the element should end at
-					// speed: 500,           // how long it should take to count between the target numbers
-					refreshInterval: 50,  // how often the element should be updated
-					decimals: 0,           // the number of decimal places to show
-					formatter: formatter,  // handler for formatting the value before rendering
-					onUpdate: null,        // callback method for every time the element is updated
-					onComplete: null       // callback method for when the element finishes updating
-				};
-				
-				function formatter(value, settings) {
-					return value.toFixed(settings.decimals);
-				}
-
-
-
-			  // custom formatting example
-			  $('#count-number').data('countToOptions', {
-				formatter: function (value, options) {
-				  return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
-				}
-			  });
-			  
-			  // start all the timers
-			  $('.timer').each(count);  
-			  
-			  function count(options) {
-				var $this = $(this);
-				options = $.extend({}, options || {}, $this.data('countToOptions') || {});
-				$this.countTo(options);
-			  }
-			//数字翻滚结束
-
-
-		}
-
-		// 在这你可以获得
-		$('.learn-con').hover(function(){
-			$(this).stop(true,false).animate({
-				'bottom':'16px',
-			},300)
-		},function(){
-			$(this).stop(true,false).animate({
-				'bottom':'0px',
-			})
-		})
-
-
-
-		// comments
-		    var swiper = new Swiper('.comments .swiper-container', {
-		        pagination: '.comments .swiper-pagination',
-		        paginationClickable: true,
-		        slidesPerView: 3,
-		        spaceBetween: 50,
-		        breakpoints: {
-		            1024: {
-		                slidesPerView: 4,
-		                spaceBetween: 40
-		            },
-		            768: {
-		                slidesPerView: 2,
-		                spaceBetween: 30
-		            },
-		            640: {
-		                slidesPerView: 1,
-		                spaceBetween: 20
-		            },
-		            320: {
-		                slidesPerView: 1,
-		                spaceBetween: 10
-		            }
-		        }
-		    });
-	})
+$(window).scroll(function(){
+// comments
+    var swiper = new Swiper('.comments .swiper-container', {
+        pagination: '.comments .swiper-pagination',
+        paginationClickable: true,
+        slidesPerView: 3,
+        spaceBetween: 50,
+        breakpoints: {
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 40
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 30
+            },
+            640: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            }
+        }
+    });
+})
 
 
